81c2f11318b849613dc85b306108dccd7b29fdfd
Vini <luisvpersy@gmail.com>
2024-06-28 11:59:21 -0300

    update host

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ app = Fastipy().cors()
 
 app.register(sheetRoutes, {"prefix": "/omie"})
 if __name__ == "__main__":
-  uvicorn.run("main:app", log_level="debug", port=80, reload=True, loop="asyncio")
+  uvicorn.run("main:app", host='0.0.0.0', port=80, reload=True, loop="asyncio")
