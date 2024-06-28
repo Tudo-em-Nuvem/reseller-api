95555a73e3bcb1aba4bea282712900025d2ca1d5
Vini <luisvpersy@gmail.com>
2024-06-28 11:36:39 -0300

    alterando porta

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ app = Fastipy().cors()
 
 app.register(sheetRoutes, {"prefix": "/omie"})
 if __name__ == "__main__":
-  uvicorn.run("main:app", log_level="debug", port=8000, reload=True, loop="asyncio")
+  uvicorn.run("main:app", log_level="debug", port=80, reload=True, loop="asyncio")
